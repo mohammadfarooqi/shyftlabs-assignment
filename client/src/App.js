6327b76a583c2feba35244b4eabc7407bd908f6b
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2023-10-20 19:04:18 -0400

    added home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from "./pages/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <RouterProvider router={router} />
     </div>
   );
 }
